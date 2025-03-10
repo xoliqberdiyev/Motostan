1c7bb26b23a9a7cd3404fe482c3eb4c53eb24025
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-03-10 15:41:18 +0500

    ff

--- product/models.py
+++ product/models.py
@@ -73,7 +73,7 @@ class Product(BaseModel):
     price = models.FloatField(default=0)
     price_type = models.CharField(max_length=250, null=True, blank=True)
     name = models.CharField(max_length=250)
-    item = models.CharField(max_length=250, null=True, blank=True)
+    item = models.CharField(max_length=250, unique=True)
     image = models.ImageField(upload_to='product/images/', null=True, blank=True)
     main_category = models.ForeignKey(MainCategory, on_delete=models.SET_NULL, related_name='products', null=True, blank=True)
     sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, related_name='products', null=True, blank=True)
