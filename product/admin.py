dd57db3664065aac836aa834df038af5d184f8e1
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-03-21 14:06:35 +0500

    fix

--- product/admin.py
+++ product/admin.py
@@ -7,13 +7,9 @@ from django.db.models import Count
 from django.urls import path
 from django.shortcuts import redirect
 from django.http import HttpResponse
-import subprocess
-
-
-
 from modeltranslation.admin import TranslationAdmin, TranslationStackedInline
-
 from product import models
+
 
 class ProductMedia(admin.TabularInline):
     model = models.ProductMedia
@@ -23,6 +19,7 @@ class ProductInfo(admin.TabularInline):
     model = models.ProductInfo
     extra = 0
 
+
 @admin.register(models.Product)
 class ProductModelAdmin(admin.ModelAdmin):
     change_list_template = "admin/custom_changelist.html"
@@ -31,7 +28,11 @@ class ProductModelAdmin(admin.ModelAdmin):
     list_filter = ['main_category']
     list_editable = ['image']
     inlines = [ProductMedia, ProductInfo]
-
+    fieldsets = (
+        ('Asosiy ma\'lumotlar', {
+            'fields': ('name', 'image')
+        }),
+    )
 
     def get_urls(self):
         urls = super().get_urls()
@@ -42,96 +43,15 @@ class ProductModelAdmin(admin.ModelAdmin):
 
     def update_data(self, request):
         file_path = "/var/www/backend/moto"
-        # venv_path = os.path.join(file_path, "venv", "bin", "activate")
         os.system(f"cd {file_path} && . venv/bin/activate && python3 utils.py")    
-        # Skriptni to‘g‘ri ishga tushirish
-        # commands = [
-            # f"cd {file_path} && source venv/bin/activate && python3 utils.py",
-            # f"cd {file_path} && source venv/bin/activate && pip3 install -r requirements.txt",
-        # ]
-
-        # for cmd in commands:
-        #     process = subprocess.run(cmd, shell=True, text=True, capture_output=True)
-        #     if process.returncode != 0:
-        #         return {"error": process.stderr}
-
         messages.success(request, "Ma'lumotlar muvaffaqiyatli yangilandi!")
         return redirect("admin:product_product_changelist")  
 
 
-
-class CategoryInline(admin.StackedInline):
-    model = models.ProductCategory
-    extra = 0
-
-    
-class SubCategory(admin.StackedInline):
-    model = models.SubCategory
-    extra = 0
-    fields = ['link', 'name']
-    readonly_fields = ['link']
-    
-    def link(self, instance):
-        url = f"/admin/product/subcategory/{instance.id}/change/"
-        return mark_safe(f'<a target="_blank" href="{url}">Kirish</a>')
-
-    
-
-@admin.register(models.SubCategory)
-class SubCategoryAdmin(admin.ModelAdmin):
-    inlines = [CategoryInline]
-    list_display = ["id", "name", 'main_category']
-    list_filter = ['main_category']
-
-    
-@admin.register(models.MainCategory)
-class MainCategoryAdmin(admin.ModelAdmin):
-    list_display = ["name",]
-    inlines =  [SubCategory]
-
-    # def products_count(self, main_category):
-    #     url = (
-    #         reverse("admin:product_product_changelist")
-    #         + '?'
-    #         + urlencode({
-    #             "main_category__id": str(main_category.id)
-    #         })    
-    #     ) 
-    #     return format_html('<a href="{}">{} products</a>',url, main_category.products_count)
-    
-    # def get_queryset(self, request):
-    #     return super().get_queryset(request).annotate(
-    #         products_count=Count('products')
-    #     )
-
-@admin.register(models.ProductCategory)
-class ProductCategoryAdmin(admin.ModelAdmin):
-    list_display = ["name",]
-    
-
-    # def products_count(self, category):
-    #     url = (
-    #         reverse("admin:product_product_changelist")
-    #         + '?'
-    #         + urlencode({
-    #             "category_id": str(category.id)
-    #         })    
-    #     ) 
-    #     return format_html('<a href="{}">{} products</a>',url, category.products_count)
-    
-    # def get_queryset(self, request):
-    #     return super().get_queryset(request).annotate(
-    #         products_count=Count('products')
-    #     )
-
-@admin.register(models.Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-
-@admin.register(models.FifthCategroy)
-class FifthCategoryAdmin(admin.ModelAdmin):
-    pass
-
 @admin.register(models.ProductInfo)
 class ProductInfoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.ProductMedia)
+class ProductMediaAdmin(admin.ModelAdmin):
     pass
