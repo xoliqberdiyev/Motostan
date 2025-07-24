a2cd785c3172edc729698bd7c24463b603baf511
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-07-24 13:58:03 +0500

    register price type on product admin

--- product/form.py
+++ product/form.py
@@ -5,7 +5,7 @@ from product import models
 class Productorm(forms.ModelForm):
     class Meta:
         model = models.Product
-        fields = ('name', 'item', 'image')
+        fields = ('name', 'item', 'image', 'price_type')
 
     class Media:
         js = ('js/custom_file_upload.js',)
